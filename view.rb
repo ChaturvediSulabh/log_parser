233dcace5b7d55abe2952140117aced34be8b4d5
ChaturvediSulabh <sulabhchaturvedi08@gmail.com>
2019-11-17 19:29:29 +0000

    add methods to modify text appearance and alignment

--- view.rb
+++ view.rb
@@ -1,9 +1,31 @@
+require 'io/console'
+
 class BasicView
+
+  def clear_display
+    print "\e[2J"
+  end
+
+  def set_cursor row = 1, column = 1
+    print "\e[#{row};#{column}H"
+  end
+
+  def center text
+    columns = $stdin.winsize[1]
+    text_length = text.length
+    column_location = columns / 2 - text_length / 2
+    "\e[#{column_location}G#{text}"
+  end
+
+  def red text
+    "\e[31;40m#{text}\e[0m"
+  end
+
 end
 
 class FileDialogView < BasicView
   def display
-    puts "Please select an apache log file"
+    puts red(center("Please select an apache log file."))
   end
 end
 
